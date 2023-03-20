c6ae285e5fe1e7a4d3a19393a4ddf718b6a12457
connorGA <ronnoccap@gmail.com>
2023-03-19 20:18:53 -0700

    add Profile model and signin singup views

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
     path('api/test/', views.test_api, name='test_api')
 ]

322f3b664b6ac065ae46bc6b2b9eff97ddc04ad4
connorGA <ronnoccap@gmail.com>
2023-03-24 18:45:32 -0700

    chore: update Asset component to handle crud operations on users Assets, still need to fix connection on backend

--- reactapp/src/components/assests/Assets.jsx
+++ reactapp/src/components/assests/Assets.jsx
@@ -1,43 +1,84 @@
-import React, { useState } from 'react';
-import './assets.css';
+import React, { useState, useEffect } from "react";
+import "./assets.css";
 
 function Assets() {
-    const [showForm, setShowForm] = useState(false);
-    const [assets, setAssets] = useState([]);
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-  
-      const newAsset = {
-        name: e.target.name.value,
-        price: e.target.price.value,
-        boughtDate: e.target.boughtDate.value,
-        soldDate: e.target.soldDate.value,
-        profitLoss: e.target.profitLoss.value,
-      };
-  
-      setAssets([...assets, newAsset]);
-      setShowForm(false);
+  const [showForm, setShowForm] = useState(false);
+  const [assets, setAssets] = useState([]);
+  const [selectedAsset, setSelectedAsset] = useState(null);
+
+  useEffect(() => {
+    fetchAssets();
+  }, []);
+
+  const fetchAssets = async () => {
+    const response = await fetch("/api/assets/");
+    const data = await response.json();
+    setAssets(data);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newAsset = {
+      name: e.target.name.value,
+      current_price: e.target.current_price.value,
+      buy_date: e.target.buy_date.value,
+      buy_price: e.target.buy_price.value,
+      sale_date: e.target.sale_date.value || null,
+      sale_price: e.target.sale_price.value || null,
+      profit_or_loss: e.target.profit_or_loss.value || null,
     };
+
+    const requestOptions = {
+      method: selectedAsset ? "PUT" : "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newAsset),
+    };
+
+    const url = selectedAsset
+      ? `/api/assets/${selectedAsset.id}/`
+      : "/api/assets/";
+
+    await fetch(url, requestOptions);
+    fetchAssets();
+    setShowForm(false);
+    setSelectedAsset(null);
+  };
+
+  const handleEdit = (asset) => {
+    setSelectedAsset(asset);
+    setShowForm(true);
+  };
+
+  const handleDelete = async (id) => {
+    await fetch(`/api/assets/${id}/`, { method: "DELETE" });
+    fetchAssets();
+  };
 
   return (
     <div>
       {showForm ? (
         <form onSubmit={handleSubmit}>
-          <label htmlFor="name">Asset Name:</label>
+                    <label htmlFor="name">Asset Name:</label>
           <input type="text" id="name" name="name" />
 
-          <label htmlFor="price">Price:</label>
-          <input type="number" id="price" name="price" />
+          <label htmlFor="current_price">Current Price:</label>
+          <input type="number" id="current_price" name="current_price" />
 
-          <label htmlFor="boughtDate">Bought Date:</label>
-          <input type="date" id="boughtDate" name="boughtDate" />
+          <label htmlFor="buy_date">Bought Date:</label>
+          <input type="date" id="buy_date" name="buy_date" />
 
-          <label htmlFor="soldDate">Sold Date:</label>
-          <input type="date" id="soldDate" name="soldDate" />
+          <label htmlFor="buy_price">Bought Price:</label>
+          <input type="number" id="buy_price" name="buy_price" />
 
-          <label htmlFor="profitLoss">Profit or Loss:</label>
-          <input type="number" id="profitLoss" name="profitLoss" />
+          <label htmlFor="sale_date">Sold Date:</label>
+          <input type="date" id="sale_date" name="sale_date" />
+
+          <label htmlFor="sale_price">Sold Price:</label>
+          <input type="number" id="sale_price" name="sale_price" />
+
+          <label htmlFor="profit_or_loss">Profit or Loss:</label>
+          <input type="number" id="profit_or_loss" name="profit_or_loss" />
 
           <button type="submit">Submit</button>
         </form>
@@ -46,21 +87,30 @@ function Assets() {
           <table>
             <thead>
               <tr>
-                <th>Asset Name</th>
-                <th>Price</th>
+                <th>Asset</th>
+                <th>Current Price</th>
                 <th>Bought Date</th>
+                <th>Bought Price</th>
                 <th>Sold Date</th>
+                <th>Sold Price</th>
                 <th>Profit or Loss</th>
+                <th>Actions</th>
               </tr>
             </thead>
             <tbody>
               {assets.map((asset, index) => (
                 <tr key={index}>
                   <td>{asset.name}</td>
-                  <td>{asset.price}</td>
-                  <td>{asset.boughtDate}</td>
-                  <td>{asset.soldDate}</td>
-                  <td>{asset.profitLoss}</td>
+                  <td>{asset.current_price}</td>
+                  <td>{asset.buy_date}</td>
+                  <td>{asset.buy_price}</td>
+                  <td>{asset.sale_date}</td>
+                  <td>{asset.sale_price}</td>
+                  <td>{asset.profit_or_loss}</td>
+                  <td>
+                    <button onClick={() => handleEdit(asset)}>Edit</button>
+                    <button onClick={() => handleDelete(asset.id)}>Delete</button>
+                  </td>
                 </tr>
               ))}
             </tbody>
@@ -69,7 +119,20 @@ function Assets() {
             Add Asset
           </button>
         </div>
+
       )}
+
+
+        {assets.map((asset) => (
+          <tr key={asset.id}>
+            {/* Table data */}
+            <td>
+              <button onClick={() => handleEdit(asset)}>Edit</button>
+              <button onClick={() => handleDelete(asset.id)}>Delete</button>
+            </td>
+          </tr>
+        ))}
+
     </div>
   );
 }

04da529cad76ba18acc05d0e1dde4fb9be6eca87
connorGA <ronnoccap@gmail.com>
2023-03-31 19:46:25 -0700

    REACT: add mainPage.css file to format components

--- reactapp/src/components/mainPage/MainPage.jsx
+++ reactapp/src/components/mainPage/MainPage.jsx
@@ -4,22 +4,18 @@ import Header from '../header/Header';
 import Assets from '../assests/Assets';
 import ProfitGraph from '../profitGraph/ProfitGraph';
 import Budget from '../budget/Budget';
+import './mainPage.css';
 
 function MainPage() {
   console.log('MainPage rendering'); // Add this line for debugging
   
-  // const data = [
-  //   { month: '2023-01-01', profit: 500 },
-  //   { month: '2023-02-01', profit: 1000 },
-  //   { month: '2023-03-01', profit: 1500 },
-  //   { month: '2023-04-01', profit: 2000 },
-  // ];
   return (
     <>
       <Header />
+      <div className="middle-container">
       <Assets />
       <ProfitGraph />
-      {/* <ProfitGraph data={data}/> */}
+      </div>
       <Budget />
     </>
   );
